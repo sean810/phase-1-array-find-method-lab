e2f14eea15a954432483b1b6a005c560542e889a
sean810 <seantashel@gmail.com>
2024-09-30 14:58:42 +0300

    complete tab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// code your solution here
+function superbowlWin(games) {
+    const win = games.find(game => game.result === "W");
+    return win ? win.year : undefined;
+}
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+console.log(superbowlWin(record));
+
